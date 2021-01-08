f57a6e11b892038adade998e9f4110c0566c0bd9
bing9 <akass191@gmail.com>
2021-01-08 21:01:46 +0900

    프로젝트 생성

---
+++ src/main/java/com/wemakeprice/htmldivision/HtmlDivisionApplication.java
@@ -0,0 +1,13 @@
+package com.wemakeprice.htmldivision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlDivisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlDivisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wemakeprice/htmldivision/HtmlDivisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemakeprice.htmldivision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlDivisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
