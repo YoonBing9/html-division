74076483163d075023cc81f721c03ab00d555da4
YoonBing9 <akass191@gmail.com>
2021-01-09 19:49:27 +0900

    서비스 단위 테스트 보강

--- src/main/java/com/wemakeprice/htmldivision/online/domain/dto/HtmlDivisionOutDto.java
+++ src/main/java/com/wemakeprice/htmldivision/online/domain/dto/HtmlDivisionOutDto.java
@@ -2,19 +2,19 @@ package com.wemakeprice.htmldivision.online.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @ToString
+@Getter
 public class HtmlDivisionOutDto {
     private String quotient;
     private String remainder;
-    private String errorMsg;
 
     @Builder
-    public HtmlDivisionOutDto(String quotient, String remainder, String errorMsg) {
+    public HtmlDivisionOutDto(String quotient, String remainder) {
         this.quotient = quotient;
         this.remainder = remainder;
-        this.errorMsg = errorMsg;
     }
 }
--- src/test/java/com/wemakeprice/htmldivision/online/service/HtmlDivisionServiceImplTest.java
+++ src/test/java/com/wemakeprice/htmldivision/online/service/HtmlDivisionServiceImplTest.java
@@ -1,9 +1,11 @@
 package com.wemakeprice.htmldivision.online.service;
 
 import com.wemakeprice.htmldivision.online.domain.dto.HtmlDivisionInDto;
+import com.wemakeprice.htmldivision.online.domain.dto.HtmlDivisionOutDto;
 import org.assertj.core.api.Assertions;
 import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -34,11 +36,13 @@ class HtmlDivisionServiceImplTest {
     }
 
     @Test
+    @DisplayName("URL로 HTML 가져오기")
     void getHtml() {
         Assertions.assertThat(htmlDivisionService.getHtml(testUrl)).contains("html");
     }
 
     @Test
+    @DisplayName("문자열에서 HTML 요소만 삭제")
     void removeTags() {
         String html = htmlDivisionService.getHtml(testUrl);
 
@@ -48,6 +52,7 @@ class HtmlDivisionServiceImplTest {
     }
 
     @Test
+    @DisplayName("영어만 필터링")
     void OnlyEnglishFilter() {
         String html = htmlDivisionService.getHtml(testUrl);
 
@@ -57,6 +62,7 @@ class HtmlDivisionServiceImplTest {
     }
 
     @Test
+    @DisplayName("숫자만 필터링")
     void OnlyNumberFilter() {
         String html = htmlDivisionService.getHtml(testUrl);
 
@@ -71,23 +77,54 @@ class HtmlDivisionServiceImplTest {
     }
 
     @Test
+    @DisplayName("오름차순으로 정렬된 숫자는 무조건 앞의 요소보다 크거나 같다.")
     void sortNumber() {
+        boolean isSorted = true;
         String html = htmlDivisionService.getHtml(testUrl);
         String onlyNum = htmlDivisionService.OnlyNumberFilter(html);
 
-        List list = htmlDivisionService.sortNumber(onlyNum);
+        List<String> list = htmlDivisionService.sortNumber(onlyNum);
 
+        for(int i=1; i<list.size(); i++) {
+            if(list.get(i).compareTo(list.get(i-1)) < 0) {
+                isSorted = false;
+            }
+        }
 
-        System.out.println(htmlDivisionService.sortNumber(htmlDivisionService.OnlyNumberFilter(htmlDivisionService.getHtml("https://www.naver.com"))));
+        Assertions.assertThat(isSorted).isTrue();
     }
 
     @Test
+    @DisplayName("두 리스트의 길이의 합은 섞은 리스트의 길이와 같다.")
     void mixList() {
-        System.out.println(htmlDivisionService.mixList(Arrays.asList(new String[]{"A","a","B","b","C","c"}),Arrays.asList(new String[]{"0","1","2","3","4","5"})));
+        List list1 = new ArrayList(Arrays.asList(new String[]{"A","a","B","b","C","c"}));
+        List list2 = new ArrayList(Arrays.asList(new String[]{"0","1","2","3","4","5","6","7","8"}));
+
+        List mixedList = htmlDivisionService.mixList(list1, list2);
+
+        Assertions.assertThat(list1.size() + list2.size()).isSameAs(mixedList.size());
     }
 
     @Test
+    @DisplayName("요소가 0인 리스트 섞기")
+    void mixListNoElement() {
+        List list1 = new ArrayList(Arrays.asList(new String[]{}));
+        List list2 = new ArrayList(Arrays.asList(new String[]{}));
+
+        List mixedList = htmlDivisionService.mixList(list1, list2);
+
+        Assertions.assertThat(list1.size() + list2.size()).isSameAs(mixedList.size());
+    }
+
+    @Test
+    @DisplayName("문자열의 길이를 숫자로 나눈 나머지의 길이 체크")
     void division() {
-        System.out.println(htmlDivisionService.division(10,htmlDivisionService.mixList(Arrays.asList(new String[]{"A","a","B","b","C","c"}),Arrays.asList(new String[]{"0","1","2","3","4","5"}))));
+        int divisor = 10;
+        String[] temp = new String[]{"1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"};
+
+        HtmlDivisionOutDto htmlDivisionOutDto = htmlDivisionService.division(divisor, new ArrayList<String>(Arrays.asList(temp)));
+        int mod = temp.length % divisor;
+
+        Assertions.assertThat(mod).isSameAs(htmlDivisionOutDto.getRemainder().length());
     }
 }
